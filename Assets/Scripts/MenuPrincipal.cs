150a961c97d47275e0fc5cd060cc96d058b7a6d3
brylkovtaras-cloud <brylkov.taras@gmail.com>
2026-06-20 15:32:44 +0200

    Btones Basicos del menu principal
    Puse 3 botones jugar creditos y salir,
    sin editar, de forma temporal

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
