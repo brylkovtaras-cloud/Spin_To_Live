8439abc246ab8d1c833166a821d5f2ae68e8d36b
brylkovtaras-cloud <brylkov.taras@gmail.com>
2026-06-26 20:58:35 +0200

    Added Tutorial and finished credits scene

---
+++ Assets/Scripts/CreditosManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosManager : MonoBehaviour
+{
+    
+        public void VolverMenu()
+        {
+            SceneManager.LoadScene("MenuPrincipal");
+        }
+ 
+}
--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -3,12 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    public GameObject howtoplay;
     public void Jugar()
     {
         SceneManager.LoadScene("Wip_LvL");
     }
-    
-
+    public void Controls()
+    {
+        howtoplay.SetActive(true);
+    }
+    public void CloseControls()
+    {
+        howtoplay.SetActive(false);
+    }
     public void Creditos()
     {
         SceneManager.LoadScene("Creditos");
